0af6f737e47465e3a19cd892a182c16d3d70caf0
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-03-20 04:52:25 +0300

    Update NoInternetScreen.kt

--- app/src/main/java/com/zeeko/mindclash/presentation/components/NoInternetScreen.kt
+++ app/src/main/java/com/zeeko/mindclash/presentation/components/NoInternetScreen.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zeeko.mindclash.utils.LanguageManager
-
+import androidx.compose.ui.graphics.graphicsLayer
 @Composable
 fun NoInternetScreen(
     onRetryClick: () -> Unit,
