f9145f6acbe082e6d528bbb91df98e41c6a5bcc0
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-03-22 03:07:18 +0300

    Create Color.kt

---
+++ app/src/main/java/com/zeeko/mindclash/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.zeeko.mindclash.ui.theme
+import androidx.compose.ui.graphics.Color
+
+val DarkBackground = Color(0xFF0A0E17)
+val DarkSurface = Color(0xFF131A2A)
+val NeonBlue = Color(0xFF00F0FF)
+val NeonPink = Color(0xFFFF007F)
+val NeonPurple = Color(0xFF8A2BE2)
+val NeonGreen = Color(0xFF39FF14)
+val NeonRed = Color(0xFFFF1E56)
+val Gold = Color(0xFFFFD700)
