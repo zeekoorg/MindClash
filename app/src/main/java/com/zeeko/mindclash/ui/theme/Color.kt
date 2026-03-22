12f6f00420e2960206f1324078480d5f5ecdc49c
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-03-23 02:11:35 +0300

    Update Color.kt

--- app/src/main/java/com/zeeko/mindclash/ui/theme/Color.kt
+++ app/src/main/java/com/zeeko/mindclash/ui/theme/Color.kt
@@ -1,11 +1,13 @@
 package com.zeeko.mindclash.ui.theme
+
 import androidx.compose.ui.graphics.Color
 
-val DarkBackground = Color(0xFF0A0E17)
-val DarkSurface = Color(0xFF131A2A)
-val NeonBlue = Color(0xFF00F0FF)
-val NeonPink = Color(0xFFFF007F)
-val NeonPurple = Color(0xFF8A2BE2)
-val NeonGreen = Color(0xFF39FF14)
-val NeonRed = Color(0xFFFF1E56)
-val Gold = Color(0xFFFFD700)
+// لوحة ألوان "الفخامة المظلمة" (Dark Luxury) للعبة صراع العقول
+val ObsidianBlack = Color(0xFF0B0E14) // أسود عميق جداً لخلفية اللعبة
+val MidnightBlue = Color(0xFF151A22) // أزرق ليلي للبطاقات والنوافذ الزجاجية
+val LiquidGold = Color(0xFFFFD700) // ذهبي ساطع للنجوم، التلميحات، والكؤوس
+val NeonCyan = Color(0xFF00E5FF) // سماوي نيون للحدود النشطة والمؤثرات
+val CrimsonRed = Color(0xFFFF2A55) // أحمر قرمزي للإجابات الخاطئة والقلوب
+val EmeraldGreen = Color(0xFF00FA9A) // أخضر زمردي للإجابات الصحيحة
+val TextSilver = Color(0xFFE0E0E0) // فضي ناعم للنصوص ليكون مريحاً للعين
+val GlassWhite = Color(0x1AFFFFFF) // أبيض شفاف (تأثير زجاجي) للخلفيات
