0325adb0b698535c5fb79f13ec6cb728a3541f0f
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-03-23 18:57:07 +0300

    Update Color.kt

--- app/src/main/java/com/zeeko/mindclash/ui/theme/Color.kt
+++ app/src/main/java/com/zeeko/mindclash/ui/theme/Color.kt
@@ -2,12 +2,107 @@ package com.zeeko.mindclash.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// لوحة ألوان "الفخامة المظلمة" (Dark Luxury) للعبة صراع العقول
-val ObsidianBlack = Color(0xFF0B0E14) // أسود عميق جداً لخلفية اللعبة
-val MidnightBlue = Color(0xFF151A22) // أزرق ليلي للبطاقات والنوافذ الزجاجية
-val LiquidGold = Color(0xFFFFD700) // ذهبي ساطع للنجوم، التلميحات، والكؤوس
-val NeonCyan = Color(0xFF00E5FF) // سماوي نيون للحدود النشطة والمؤثرات
-val CrimsonRed = Color(0xFFFF2A55) // أحمر قرمزي للإجابات الخاطئة والقلوب
-val EmeraldGreen = Color(0xFF00FA9A) // أخضر زمردي للإجابات الصحيحة
-val TextSilver = Color(0xFFE0E0E0) // فضي ناعم للنصوص ليكون مريحاً للعين
-val GlassWhite = Color(0x1AFFFFFF) // أبيض شفاف (تأثير زجاجي) للخلفيات
+// 🌟 لوحة الألوان "الكون المظلم" (Dark Universe) - إصدار خرافي
+
+// ⚫ الأسود المتدرج
+val ObsidianBlack = Color(0xFF030014)      // أسود كوني أعمق
+val VoidBlack = Color(0xFF010008)          // أسود الفراغ المطلق
+val DarkMatter = Color(0xFF0A0A1A)         // مادة مظلمة
+
+// 🔵 الأزرق الليلي المتطور
+val MidnightBlue = Color(0xFF0F172A)       // أزرق ليلي غامق
+val CosmicBlue = Color(0xFF1A1F2E)         // أزرق كوني
+val NebulaBlue = Color(0xFF16213E)         // أزرق السديم
+
+// 💛 الذهب المتوهج
+val LiquidGold = Color(0xFFFFD700)         // ذهب سائل أساسي
+val RoyalGold = Color(0xFFFFE55C)          // ذهب ملكي
+val AncientGold = Color(0xFFFDB827)        // ذهب قديم
+val SparkleGold = Color(0xFFFFF0A3)        // ذهب براق
+
+// 💙 النيون السماوي
+val NeonCyan = Color(0xFF00FFFF)           // سماوي نيون نقي
+val ElectricCyan = Color(0xFF33FFFF)       // سماوي كهربائي
+val DeepCyan = Color(0xFF00CCFF)           // سماوي عميق
+
+// ❤️ الأحمر القرمزي
+val CrimsonRed = Color(0xFFFF2A55)         // أحمر قرمزي أساسي
+val BloodRed = Color(0xFFFF1F3F)           // أحمر دموي
+val HeartGlow = Color(0xFFFF6B6B)          // توهج القلب
+
+// 💚 الأخضر الزمردي
+val EmeraldGreen = Color(0xFF2ECC71)       // زمردي نقي
+val MintGreen = Color(0xFF3BE477)          // نعناعي ساطع
+val SuccessGreen = Color(0xFF00FF7F)       // أخضر نجاح
+
+// 🎨 ألوان النيون المتعددة
+val NeonMagenta = Color(0xFFFF00FF)        // أرجواني نيون
+val NeonOrange = Color(0xFFFF5F00)         // برتقالي نيون
+val NeonPurple = Color(0xFFAA00FF)         // بنفسجي نيون
+val NeonPink = Color(0xFFFF10F0)           // وردي نيون
+
+// ✨ الأبيض والفضي
+val TextSilver = Color(0xFFE8E8E8)         // فضي نقي
+val DiamondWhite = Color(0xFFF5F5F5)       // أبيض ألماس
+val PearlWhite = Color(0xFFF0F0F0)         // أبيض لؤلؤي
+
+// 🪟 تأثيرات الزجاج
+val GlassWhite = Color(0x33FFFFFF)         // زجاج أبيض
+val GlassGold = Color(0x33FFD700)          // زجاج ذهبي
+val GlassCyan = Color(0x3300FFFF)          // زجاج سماوي
+val GlassPurple = Color(0x33AA00FF)        // زجاج بنفسجي
+
+// 🌟 ألوان متدرجة متحركة
+val GradientStart = Color(0xFF667EEA)      // بداية التدرج
+val GradientMiddle = Color(0xFF764BA2)     // وسط التدرج
+val GradientEnd = Color(0xFFF093FB)        // نهاية التدرج
+
+// 🏆 ألوان الإنجازات
+val Platinum = Color(0xFFE5E4E2)           // بلاتينيوم
+val Diamond = Color(0xFFB9F2FF)            // ألماس
+val Ruby = Color(0xFFFF1F3F)               // ياقوت
+
+// 🌈 ألوان قوس قزح
+val RainbowRed = Color(0xFFFF0000)
+val RainbowOrange = Color(0xFFFF7F00)
+val RainbowYellow = Color(0xFFFFFF00)
+val RainbowGreen = Color(0xFF00FF00)
+val RainbowBlue = Color(0xFF0000FF)
+val RainbowIndigo = Color(0xFF4B0082)
+val RainbowViolet = Color(0xFF9400D3)
+
+// ⚡ ألوان الطاقة
+val EnergyBlue = Color(0xFF0A66FF)
+val EnergyYellow = Color(0xFFFFE500)
+val EnergyRed = Color(0xFFFF3131)
+
+// 🌸 ألوان سحرية
+val MagicPurple = Color(0xFF9D4EDD)
+val MagicPink = Color(0xFFFF66CC)
+val MagicBlue = Color(0xFF3B82F6)
+
+// 💀 ألوان التحذير
+val WarningRed = Color(0xFFDC2626)
+val WarningOrange = Color(0xFFF97316)
+
+// 🎭 ألوان الظل والتوهج
+val ShadowBlack = Color(0x4D000000)
+val GlowWhite = Color(0x33FFFFFF)
+val GlowGold = Color(0x55FFD700)
+
+// 🔥 ألوان النار
+val FireRed = Color(0xFFFF4500)
+val FireOrange = Color(0xFFFF8C42)
+
+// 🧊 ألوان الجليد
+val IceBlue = Color(0xFF00BFFF)
+val IceWhite = Color(0xFFE0FFFF)
+
+// 🌊 ألوان المحيط
+val OceanBlue = Color(0xFF006994)
+val WaveBlue = Color(0xFF48C9B0)
+
+// 🌅 ألوان الغروب
+val SunsetRed = Color(0xFFFF4D4D)
+val SunsetOrange = Color(0xFFFF8C42)
+val SunsetGold = Color(0xFFFFD966)
