0f89c8bdff96caf7a2812fa3c6ce9990a66fc90d
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-03-27 20:17:23 +0300

    Update StoreScreen.kt

--- app/src/main/java/com/zeeko/mindclash/ui/screens/StoreScreen.kt
+++ app/src/main/java/com/zeeko/mindclash/ui/screens/StoreScreen.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.ui.draw.scale
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
